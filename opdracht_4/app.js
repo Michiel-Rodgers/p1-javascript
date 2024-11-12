ba7f23751f7f789d98c19a1838984596dc176e14
Michiel R <michielrodgers@gmail.com>
2024-11-12 14:46:57 +0100

    Opdracht_4

---
+++ opdracht_4/app.js
@@ -0,0 +1,16 @@
+let title = document.getElementById("title");
+
+function red() {
+    console.log("de functie werkt");
+    title.style.color = "red";
+}
+
+function yellow () {
+    console.log("de functie werkt");
+    title.style.color = "yellow";
+}
+
+function blue () {
+    console.log("de functie werkt");
+    title.style.color = "blue";
+}
