260dd639578bc24bd2641b145a3e02ef24704cfb
Michiel R <michielrodgers@gmail.com>
2024-11-12 15:26:58 +0100

    Opdracht_6

---
+++ opdracht_6/app.js
@@ -0,0 +1,17 @@
+function calculate(num1, num2) {
+    console.log(num1 + num2)
+}
+
+calculate(1, 120)
+
+function multiplyByFive(num1){
+console.log(num1 * 5)
+}
+
+multiplyByFive(7)
+
+function calculateMinutes(num1){
+    console.log(num1 / 60)
+}
+
+calculateMinutes(15000)
