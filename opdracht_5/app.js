1af6d8dfa95a5e34caf7bc831ce763dd22df7dd0
Michiel R <michielrodgers@gmail.com>
2024-11-12 14:55:38 +0100

    Opdracht_5

---
+++ opdracht_5/app.js
@@ -0,0 +1,10 @@
+let title = document.getElementById("title");
+
+function changeColor() {
+    console.log("De functie werkt!");
+    red();
+}
+
+function red() {
+    title.style.color = "red";
+}
